a1726b6dfa94e8bd46e78867c8b4f7f21fbbc9c7
liujiapeng678 <jj0723938@gmail.com>
2024-11-04 21:27:43 +0800

    加入蛇

--- src/assets/scripts/GameMap.js
+++ src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { AcGameObject } from "./AcGameObject";
 import { Wall } from "./Wall";
+import { Snake } from "./Snake";
 
 export class GameMap extends AcGameObject {
     constructor(ctx, parent){
@@ -9,11 +10,49 @@ export class GameMap extends AcGameObject {
         this.parent = parent
         this.L = 0
         this.rows = 13
-        this.cols = 13
+        this.cols = 14
 
         this.walls = []
         this.inner_walls_count = 26
+        this.snakes = [
+            new Snake({id: 0, color: "#4876EC", r: this.rows - 2, c: 1}, this),
+            new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2}, this),
+        ]
     }
+
+    add_listening_events(){
+        this.ctx.canvas.focus()
+
+        const [snake0, snake1] = this.snakes
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if(e.key === 'w'){
+                snake0.set_direction(0)
+            } else if(e.key === 'd'){
+                snake0.set_direction(1)
+            } else if(e.key === 's'){
+                snake0.set_direction(2)
+            } else if(e.key === 'a'){
+                snake0.set_direction(3)
+            } else if(e.key === 'ArrowUp'){
+                snake1.set_direction(0)
+            } else if(e.key === 'ArrowRight'){
+                snake1.set_direction(1)
+            } else if(e.key === 'ArrowDown'){
+                snake1.set_direction(2)
+            } else if(e.key === 'ArrowLeft'){
+                snake1.set_direction(3)
+            }
+        })
+    }
+
+    check_ready(){      //  两条蛇是否都准备好下一步，必须满足当前静止，且有下一步的方向
+        for(const snake of this.snakes){
+            if(snake.status !== "idle")return false
+            if(snake.direction === -1)return false
+        }
+        return true
+    }
+
     check_connect(is_wall, sx, sy, tx, ty){
         if(sx == tx && sy == ty)return true
         is_wall[sx][sy] = true
@@ -46,9 +85,9 @@ export class GameMap extends AcGameObject {
             for(let j = 0; j < 1000; j++){
                 let r = parseInt(Math.random() * this.rows)
                 let c = parseInt(Math.random() * this.cols)
-                if(is_wall[r][c] || is_wall[c][r])continue
+                if(is_wall[r][c] || is_wall[this.rows - 1 - r][this.cols - 1 - c])continue
                 if(r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2)continue
-                is_wall[r][c] = is_wall[c][r] = true
+                is_wall[r][c] = is_wall[this.rows - 1 - r][this.cols - 1 - c] = true
                 break;
             }
         }
@@ -75,6 +114,8 @@ export class GameMap extends AcGameObject {
                 break;
             }
         }
+
+        this.add_listening_events()
     }
 
     update_size(){
@@ -83,8 +124,17 @@ export class GameMap extends AcGameObject {
         this.ctx.canvas.height = this.L * this.rows
     }
 
+    next_step(){   //让两条蛇进入下一回合
+        for(const snake of this.snakes){
+            snake.next_step()
+        }
+    }
+
     update(){
         this.update_size()
+        if(this.check_ready()){
+            this.next_step()
+        }
         this.render()
     }
     
---
+++ src/assets/scripts/Snake.js
@@ -0,0 +1,91 @@
+import { AcGameObject } from "./AcGameObject";
+import { Cell } from "./Cell";
+
+export class Snake extends AcGameObject {
+    constructor(info, gamemap){
+        super()
+
+        this.id = info.id
+        this.color = info.color
+        this.gamemap = gamemap
+
+        this.cells = [new Cell(info.r, info.c)] // 蛇的身体，由一个个Cell组成
+        this.speed = 5
+        this.direction = -1  // 0 1 2 3上右下左
+        this.status = "idle"  // idle, move, die
+
+        this.next_cell = null
+        this.dr = [-1, 0, 1, 0]
+        this.dc = [0, 1, 0, -1]
+        this.step = 0   // 回合数
+
+        this.eps = 1e-2
+    }
+
+    check_tail_increasing(){  // 检测蛇长度增加
+        if(this.step <= 10)return true
+        if(this.step % 3 === 1)return true
+        return false
+    } 
+
+    set_direction(d){
+        this.direction = d
+    }
+
+    start(){
+
+    }
+    next_step(){   //更新蛇的状态到下一步
+        const d = this.direction
+        this.next_cell = new Cell(this.cells[0].r + this.dr[d], this.cells[0].c + this.dc[d])
+        this.direction = -1
+        this.status = "move"
+        this.step++
+
+        const len = this.cells.length
+        for(let i = len; i > 0; i--){
+            this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]))
+        }
+
+    }
+    update_move(){
+        const dx = this.next_cell.x - this.cells[0].x, dy = this.next_cell.y - this.cells[0].y 
+        const distance = Math.sqrt(dx * dx + dy * dy)
+        if(distance < this.eps){   // 到达目的地
+            this.cells[0] = this.next_cell
+            this.next_cell = null
+            this.status = "idle"
+
+            if(!this.check_tail_increasing()){
+                this.cells.pop()   //  蛇长不变，要去掉蛇尾
+            }
+        } else {
+            const move_distance = this.speed * this.timedelta / 1000 // 每帧距离
+            this.cells[0].x += move_distance * dx / distance
+            this.cells[0].y += move_distance * dy / distance
+            if(!this.check_tail_increasing()){
+                const len = this.cells.length
+                const tail = this.cells[len - 1], tail_target = this.cells[len - 2]
+                const tail_dx = tail_target.x - tail.x, tail_dy = tail_target.y - tail.y
+                tail.x += move_distance * tail_dx / distance
+                tail.y += move_distance * tail_dy / distance
+            }
+        }
+    }
+    update(){
+        if(this.status === 'move'){
+            this.update_move()
+        }
+        this.render()
+    }
+    render(){
+        const L = this.gamemap.L
+        const ctx = this.gamemap.ctx
+        ctx.fillStyle = this.color
+        for(const cell of this.cells){
+            ctx.beginPath()
+            ctx.arc(cell.x * L, cell.y * L, L / 2, 0, Math.PI * 2)
+            ctx.fill()
+        }
+    }
+}
