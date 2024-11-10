54d66bceca82bf8c1795857ca57418094162a3fb
liujiapeng678 <jj0723938@gmail.com>
2024-11-10 16:20:35 +0800

    前端页面的授权和跳转

--- src/router/index.js
+++ src/router/index.js
@@ -6,39 +6,64 @@ import RecordIndexView from '@/views/record/RecordIndexView.vue'
 import UserBotsIndexView from '@/views/user/bots/UserBotsIndexView.vue'
 import UserAccountLoginView from '@/views/user/account/UserAccountLoginView.vue'
 import UserAccountRegisterView from '@/views/user/account/UserAccountRegisterView.vue'
+import store from '@/store/index'
 
 const routes = [
   {
     path: "/",
-    redirect: "/pk/"
+    redirect: "/pk/",
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/pk/",
     component: PkIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/record/",
     component: RecordIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/ranklist/",
     component: RankListIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/user/bots/",
     component: UserBotsIndexView,
+    meta: {
+      requestAuth: true
+    }
   },
   {
     path: "/user/account/login/",
-    component: UserAccountLoginView
+    component: UserAccountLoginView,
+    meta: {
+      requestAuth: false
+    }
   },
   {
     path: "/user/account/register",
-    component: UserAccountRegisterView
+    component: UserAccountRegisterView,
+    meta: {
+      requestAuth: false
+    }
   },
   {
     path: "/404/",
     component: NotFoundIndexView,
+    meta: {
+      requestAuth: false
+    }
   },
   {
     path: "/:catchAll(.*)",
@@ -49,6 +74,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requestAuth && !store.state.is_login){
+    console.log(store.state.is_login)
+    next("/user/account/login/") 
+  } else {
+    next()
+  }
 })
 
 export default router
